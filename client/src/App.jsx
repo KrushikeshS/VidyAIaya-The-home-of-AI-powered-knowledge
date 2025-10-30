9075e70b0cc32ea99c5d8f0dde6f05b4d26040b4
KrushikeshS <krushikeshshelar@gmail.com>
2025-10-30 21:30:43 +0530

    feat(client): Initial React (Vite) frontend structure

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Text-to-Learn Generator</h1>
+      <p>Hello from the frontend!</p>
+    </div>
+  );
+}
+
+export default App;
